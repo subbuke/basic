de8c1917cd4b41a4b61c9d62016f285028741591
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-05 12:29:21 +0000

    started

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const cors = require("cors")
+
+app.use(cors())
+app.use(express.json)
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+app.get("/test", (req, res) => {
+         res.send('tested route successfully');
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
