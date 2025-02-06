1c0e219005605a5329bbd8627814310eaf7055d0
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-02-06 03:56:19 +0000

    database connection added

--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,13 @@ app.get("/test", (req, res) => {
          res.send('tested route successfully');
 })
 
+mongoose.connect('mongodb+srv://subramanyamchowdam7654:subbu1919@cluster2.0ybx9.mongodb.net/project?retryWrites=true&w=majority&appName=Cluster2')
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
 
 
 app.listen(PORT, () => {
